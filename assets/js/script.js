2c9de91872e99f687056cbcd62047b6cd770f73f
rfabreu <raabreugomes@gmail.com>
2022-09-23 10:18:59 -0400

    Wrote logic to create search form element

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,47 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&a
 // Makes call to the API data => Keep to use later
 // fetch(queryURL)
 
+// Get form element value
+let leftColumnEl = document.querySelector("#left-column");
+// Get all elements from cities list
+let citiesListContainerBtnEl = document.querySelector(".list-group-item");
+// Forecast container for the day
+let dailyForecastContainerEl = document.querySelector("#forecast-container");
+
+// Create form container and children child elements
+let dynFormContainer = document.createElement("form");
+dynFormContainer.setAttribute("id", "dynCityForm"); // CHECK THIS LINE LATER ON
+dynFormContainer.classList = "search-container";
+leftColumnEl.appendChild(dynFormContainer);
+
+// Create an h3 element
+let formH3 = document.createElement("h3");
+formH3.textContent = "Search for a City";
+dynFormContainer.appendChild(formH3);
+
+// Create an input element
+let formInput = document.createElement("input");
+formInput.setAttribute("id", "city-name");
+formInput.setAttribute("type", "text");
+formInput.setAttribute("autofocus", "true");
+formInput.classList = "form-input";
+dynFormContainer.appendChild(formInput);
+
+// Create button element
+let formButton = document.createElement("button");
+formButton.setAttribute("type", "submit");
+formButton.classList("btn fas fa-search");
+dynFormContainer.appendChild(formButton);
+
+// Find city form
+let searchEventHandlerEl = document.querySelector("#dynCityForm");
+let searchByCityEl = document.querySelector("#city-name");
+
+// Left column container for cities
+let citiesContainerEl = document.createElement("div");
+citiesContainerEl.setAttribute("id", "dyn-cities-list");
+citiesContainerEl.classList = "list-group";
+leftColumnEl.appendChild(citiesContainerEl);
+
+// Find list div container
+let citiesListContainerEl = document.querySelector("#dyn-cities-list");

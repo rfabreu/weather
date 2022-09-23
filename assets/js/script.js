5db3303db5a62393c102cb968a5598b9a6ba29e2
rfabreu <raabreugomes@gmail.com>
2022-09-23 09:41:52 -0400

    Set API call variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var APIKey = "f04790a4f07975aa0a32273581fb57ac";
+var city;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+// Makes call to the API data => Keep to use later
+// fetch(queryURL)
+

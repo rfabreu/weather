839f82e7337528654eda9e9416ed6719b88c5002
rfabreu <raabreugomes@gmail.com>
2022-09-23 21:48:44 -0400

    Created logic to return current and 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ let leftColumnEl = document.querySelector("#left-column");
 // Get all elements from cities list
 let citiesListContainerBtnEl = document.querySelector(".list-group-item");
 // Forecast container for the day
-let dailyForecastContainerEl = document.querySelector("#forecast-container");
+let dailyWeatherContainerEl = document.querySelector("#forecast-container");
 
 // Create form container and children child elements
 let dynFormContainer = document.createElement("form");
@@ -100,3 +100,119 @@ function fetchSecondCall(searchByCity, latNum, lonNum, unixTimeCurrentDay, curre
             populate5DayForecast(secondCallData);
         });
 };
+
+// Function to populate forecast for the current day
+function populateCurrentDayHtml(searchByCity, fullDayDaily, currentDayIcon, currentTempMetric, currentHumidity, currentMPS, mphWindSpeed, uvIndex) {
+    // Populate current day html elements
+    let dailyForecastContainerEl = document.createElement("div");
+    dailyForecastContainerEl.setAttribute("id", "daily-forecast-container");
+    dailyForecastContainerEl.classList = "borderDiv";
+
+    let currentDayTitle = document.createElement("h3");
+    currentDayTitle.textContent = (searchByCity.charAt(0).toUpperCase() + searchByCity.slice(1) + " " + fullDayDaily);
+
+    let currentIconEl = document.createElement("span");
+    let currentIconSymbol = "http://openweathermap.org/img/wn/"+ currentDayIcon + "@2x.png";
+    currentIconEl.innerHTML = "<img src=" + currentIconSymbol + "></img";
+    currentDayTitle.append(currentIconEl);
+
+    // Create <p> elements to hold current day informmation
+    let currentTempEl = document.createElement("p");
+    let currentHumidityEl = document.createElement("p");
+    let currentWindSpeedEl = document.createElement("p");
+    let currentUvIEl = document.createElement("p");
+
+    // Rund temp to no decimal places
+    // Injects <h> elements text content
+    currentTempEl.textContent = "Temperature: " + (currentTempMetric.toFixed(1)) + " ˚C";
+    currentHumidityEl.textContent = "Humidity: " + currentHumidity + "%";
+    currentWindSpeedEl.textContent = "Wind Speed: " + currentMPS + " MPH";
+    currentUvIEl.textContent = "UV Index: " + uvIndex;
+
+    // Use jQuery to clear all list items
+    $("daily-forecast-container").remove();
+
+    // Append daily forecast to forecast-container
+    dailyWeatherContainerEl.appendChild(dailyForecastContainerEl);
+    dailyForecastContainerEl.appendChild(currentDayTitle);
+    dailyForecastContainerEl.appendChild(currentTempEl);
+    dailyForecastContainerEl.appendChild(currentHumidityEl);
+    dailyForecastContainerEl.appendChild(currentWindSpeedEl);
+    dailyForecastContainerEl.appendChild(currentUvIEl);
+};
+
+function populate5DayForecast(secondCallData) {
+    $("#weekly-forecast-container").remove();
+
+    let weeklyForecastContainerEl = document.createElement("div");
+    weeklyForecastContainerEl.setAttribute("id", "weekly-forecast-container");
+    weeklyForecastContainerEl.classList = "border-Div-right-column";
+
+    let fiveDayForecast = document.createElement("h3");
+    fiveDayForecast.textContent = "5-Day Forecast:";
+
+    // Append element as topmost before loop to generate content for each <div>
+    dailyWeatherContainerEl.appendChild(weeklyForecastContainerEl);
+    weeklyForecastContainerEl.appendChild(fiveDayForecast);
+
+    // Generate <div> to hold the five days as a flex row
+    let weeklyFlexContainerEl = document.createElement("div");
+    weeklyFlexContainerEl.classList = "weekly-flex-container";
+
+    // Append element only after the date on the five day forecast
+    weeklyForecastContainerEl.appendChild(weeklyFlexContainerEl);
+
+    // Get five days content from the five day forecast
+    for (i = 1; i <= 5; i++) {
+        let unixTime = secondCallData.daily[i].dt;
+
+        let unix_timestamp = unixTime;
+        // Create a new JS date object based on the timestamp
+        // multiply it by 1000 so the argument is in milliseconds
+        var date = new Date(unix_timestamp * 1000);
+        // Hours from timestamp
+        var year = date.getFullYear();
+        var monthOfYear = date.getMonth() + 1;
+        var dayOfMonth = date.getDate();
+
+        // Values that will be displayed
+        var fullDay = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+        var iconWeather = secondCallData.daily[i].weather[0].icon;
+        let celsiusTemp = secondCallData.daily[i].temp.day;
+        let humidity = secondCallData.daily[i].humidity;
+
+        // Create five day elements
+        // Create <div> element to hold each of the five day weekly forecast
+        let eachDayContainer = document.createElement("div");
+        eachDayContainer.setAttribute("id", ("day=" + [i]));
+        eachDayContainer.classList = "border-div-five-day-forecast";
+
+        let currentDayTitle = document.createElement("p");
+        currentDayTitle.textContent = (fullDay);
+
+        // Create <span> to hold the icon
+        let iconSpan = document.createElement("p");
+        iconSpan.textContent = "";
+
+        let currentIconEl = document.createElement("span");
+        let currentIconSymbol = "http://openweathermap.org/img/wn/"+ iconWeather + "@2x.png";
+        currentIconEl.innerHTML = "<img src=" + currentIconSymbol + "></img>";
+        iconSpan.append(currentIconEl);
+
+        // Create <p> elements to hold remaining current day info
+        let currentTempEl = document.createElement("p");
+        let currentHumidityEl = document.createElement("p");
+
+        currentTempEl.textContent = "Temperature: " + (celsiusTemp.toFixed(2)) + " ˚C";
+        currentHumidityEl.textContent = "Humidity: " + humidity + "%";
+
+        // Append daily forecast to forecast container
+        eachDayContainer.appendChild(currentDayTitle);
+        eachDayContainer.appendChild(currentIconEl);
+        eachDayContainer.appendChild(currentTempEl);
+        eachDayContainer.appendChild(currentHumidityEl);
+        // Append items to the parent element
+        weeklyFlexContainerEl.appendChild(eachDayContainer);
+    };
+};
+
